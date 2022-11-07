bd13eccb9aeff53f4a50344fea1ead5ff82902c1
Paddy McCann <pgm5@st-andrews.ac.uk>
2022-11-07 14:10:59 +0000

    makes CollectionTree list more accessible
    Helps to avoid repeated link text for different destinations when there are similarly named collections in different parts of the hierarchy.

--- views/helpers/CollectionTreeFullList.php
+++ views/helpers/CollectionTreeFullList.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Collection Tree
- * 
+ *
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
@@ -31,11 +31,12 @@ class CollectionTree_View_Helper_CollectionTreeFullList extends Zend_View_Helper
             $html .= '<li>';
             if ($linkToCollectionShow) {
                 $html .= link_to_collection(null, array(), 'show', $collectionTable->find($rootCollection['id']));
+                $html .= link_to_collection(null, ["aria-label" => $rootCollection["name"]], 'show', $collectionTable->find($rootCollection['id']));
             } else {
                 $html .= $rootCollection['name'] ? $rootCollection['name'] : __('[Untitled]');
             }
             $collectionTree = get_db()->getTable('CollectionTree')->getDescendantTree($rootCollection['id']);
-            $html .= $this->view->collectionTreeList($collectionTree, $linkToCollectionShow);
+            $html .= $this->view->collectionTreeList($collectionTree, $linkToCollectionShow, $rootCollection["name"]);
             $html .= '</li>';
         }
         $html .= '</ul></div>';
